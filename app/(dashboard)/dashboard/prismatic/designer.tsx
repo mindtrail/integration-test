ca8d2ca0aaf2f479c181c7848578c5965a467ba7
Alex <alexandru.pausan@gmail.com>
2024-07-24 00:24:59 +0300

    Refactor getIntegrationId function and update UI in EmbeddedDesigner component

--- app/(dashboard)/dashboard/prismatic/designer.tsx
+++ app/(dashboard)/dashboard/prismatic/designer.tsx
@@ -7,26 +7,34 @@ const id = 'my-embedded-builder-div'
  * Get an integration's ID given its name
  */
 async function getIntegrationId(integrationName: string) {
-  const query = `query getMyIntegration($integrationName: String!) {
-  integrations(name: $integrationName) {
-    nodes {
-      id
-      name
-    }
+  const query = `query getMyIntegration {
+    integrations{
+      nodes {
+        id
+        name
+      }
   }
 }`
 
   const variables = { integrationName }
   const result = await prismatic.graphqlRequest({ query, variables })
-  return result.data.integrations.nodes?.[0].id
-}
+  console.log(222, result)
 
-const INTEGRATION_ID = 'SW50ZWdyYXRpb246ZmRjNGExOTEtNGIxOC00MTJhLTk3OWYtYjUxZWZjYzE2MmJj'
+  if (!result.data.integrations.nodes?.length) {
+    return 'null'
+  }
+  return result.data.integrations.nodes?.[0]?.id
+}
 
 function EmbeddedDesigner() {
   useEffect(() => {
     const showDesigner = async () => {
-      const integrationId = INTEGRATION_ID
+      const res = await fetch(`http://localhost:3000/api/prismatic`)
+      const data = await res.json()
+      await prismatic.authenticate({ token: data.token })
+
+      const integrationId = await getIntegrationId('Integration 1')
+      console.log(333, integrationId)
       prismatic.showDesigner({
         selector: `#${id}`,
         integrationId,
@@ -36,7 +44,11 @@ function EmbeddedDesigner() {
     showDesigner()
   }, [])
 
-  return <div id={id}>Loading...</div>
+  return (
+    <div className="h-[700px] w-full" id={id}>
+      Loading...
+    </div>
+  )
 }
 
 export default EmbeddedDesigner
--- app/(dashboard)/dashboard/prismatic/page.tsx
+++ app/(dashboard)/dashboard/prismatic/page.tsx
@@ -5,6 +5,8 @@ import { Breadcrumbs } from '@/components/breadcrumbs'
 import { Heading } from '@/components/ui/heading'
 import prismatic from '@prismatic-io/embedded'
 
+import EmbeddedDesigner from './designer'
+
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
   { title: 'Prismatic', link: '/dashboard/prismatic' },
@@ -17,6 +19,7 @@ export default function Page() {
     const fetchData = async () => {
       const res = await fetch(`http://localhost:3000/api/prismatic`)
       const data = await res.json()
+
       await prismatic.authenticate({ token: data.token })
       prismatic.showMarketplace({ selector: '#marketplace' })
     }
@@ -24,6 +27,7 @@ export default function Page() {
     const showComponents = async () => {
       const res = await fetch(`http://localhost:3000/api/prismatic`)
       const data = await res.json()
+
       await prismatic.authenticate({ token: data.token })
       prismatic.showComponents({
         selector: `#builder`,
@@ -31,21 +35,23 @@ export default function Page() {
     }
 
     prismatic.init()
-    // fetchData();
+    fetchData()
     showComponents()
   }, [])
 
   return (
     <>
-      <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+      <div className="max-h-full flex-1 space-y-4 overflow-auto p-4 pt-6 md:p-8">
         <Breadcrumbs items={breadcrumbItems} />
         <div className="flex items-start justify-between">
           <Heading title={`Prismatic`} description="Manage prismatic integrations" />
         </div>
         <h1>Prismatic</h1>
-        {/* <div className="h-[800px] w-full" id="marketplace"></div> */}
+        <div className="h-[700px] w-full" id="marketplace"></div>
         <h1>Builder</h1>
-        <div className="h-[800px] w-full" id={'builder'}></div>
+        <div className="h-[700px] w-full" id={'builder'}></div>
+        <h1>Designer</h1>
+        <EmbeddedDesigner />
       </div>
     </>
   )
